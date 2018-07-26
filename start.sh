24bb684701d0b5cde88548c99f453dd803bbca1b
jidanaichixihongshi <m15175060094@163.com>
2018-07-26 15:31:49 +0800

    ff

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ NODE="landlords"
 NODENAME="$NODE@$localip"
 
 erl -pa ebin deps/*/ebin\
-        -config config/sys.config \
+		-config config/sys.config \
 		-name $NODENAME \
 		-setcookie landlords_server \
 		-s landlords
