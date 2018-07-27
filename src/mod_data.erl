cfc28d5e93f1baa52d72749dbe18bcbf405a84f2
jidanaichixihongshi <m15175060094@163.com>
2018-07-27 16:10:21 +0800

    protobuf

---
+++ src/mod_data.erl
@@ -0,0 +1,23 @@
+%%%-------------------------------------------------------------------
+%%% @author Administrator
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. 六月 2018 18:14
+%%%-------------------------------------------------------------------
+
+-module(mod_data).
+-auth("cw").
+
+-include("file.hrl").
+
+-export([
+	start/0]).
+
+%% 生成相应的erl和hrl文件
+start() ->
+	protobuffs_compile:scan_file(?PROTO_CONFIG).
+
+
+
