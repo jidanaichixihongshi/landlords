3667a2d5ca3a3a0d97b9a8ed10317a16cc69bbdd
caowei <caowei@localhost.(none)>
2018-09-17 10:21:39 +0800

    update

---
+++ include/protobuf_pb.hrl
@@ -0,0 +1,80 @@
+-ifndef(USERDATA_PB_H).
+-define(USERDATA_PB_H, true).
+-record(userdata, {
+    nickname,
+    uid,
+    phone,
+    token = erlang:error({required, token}),
+    device = erlang:error({required, device}),
+    device_id,
+    version = erlang:error({required, version}),
+    app_id = erlang:error({required, app_id})
+}).
+-endif.
+
+-ifndef(REQUESTLOGON_PB_H).
+-define(REQUESTLOGON_PB_H, true).
+-record(requestlogon, {
+    mt = erlang:error({required, mt}),
+    mid = erlang:error({required, mid}),
+    sig = erlang:error({required, sig}),
+    timestamp = erlang:error({required, timestamp}),
+    data = erlang:error({required, data})
+}).
+-endif.
+
+-ifndef(RESPONSELOGON_PB_H).
+-define(RESPONSELOGON_PB_H, true).
+-record(responselogon, {
+    mt = erlang:error({required, mt}),
+    mid = erlang:error({required, mid}),
+    sig = erlang:error({required, sig}),
+    timestamp = erlang:error({required, timestamp}),
+    data
+}).
+-endif.
+
+-ifndef(LOGONSUCCESS_PB_H).
+-define(LOGONSUCCESS_PB_H, true).
+-record(logonsuccess, {
+    mt = erlang:error({required, mt}),
+    mid = erlang:error({required, mid}),
+    sig = erlang:error({required, sig}),
+    timestamp = erlang:error({required, timestamp}),
+    data
+}).
+-endif.
+
+-ifndef(RESPONSESESSION_PB_H).
+-define(RESPONSESESSION_PB_H, true).
+-record(responsesession, {
+    mt = erlang:error({required, mt}),
+    mid = erlang:error({required, mid}),
+    sig = erlang:error({required, sig}),
+    timestamp = erlang:error({required, timestamp}),
+    data
+}).
+-endif.
+
+-ifndef(SESSIONSUCCESS_PB_H).
+-define(SESSIONSUCCESS_PB_H, true).
+-record(sessionsuccess, {
+    mt = erlang:error({required, mt}),
+    mid = erlang:error({required, mid}),
+    sig = erlang:error({required, sig}),
+    timestamp = erlang:error({required, timestamp}),
+    data
+}).
+-endif.
+
+-ifndef(HEARTBEAT_PB_H).
+-define(HEARTBEAT_PB_H, true).
+-record(heartbeat, {
+    mt = erlang:error({required, mt}),
+    mid = erlang:error({required, mid}),
+    sig = erlang:error({required, sig}),
+    timestamp = erlang:error({required, timestamp}),
+    data
+}).
+-endif.
+
