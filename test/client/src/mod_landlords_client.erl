327ee59ed2e24da7fd40ea8bb4590740d10156a3
jidanaichixihongshi <m15175060094@163.com>
2018-09-26 15:41:06 +0800

    update

--- test/client/src/mod_landlords_client.erl
+++ test/client/src/mod_landlords_client.erl
@@ -147,8 +147,13 @@ handle_msg(#proto{mt = Mt, sig = 2, data = Data} = Msg,
 			Response = msg:produce_msg(Uid, <<"">>, Mt, Reply),
 			tcp_send(Socket, Response);
 		107 ->
-			#chat{from = From, c = C} = binary_to_term(Data),
-			io:format("======== chat ======= ~p ::: ~p~n", [binary_to_term(From), binary_to_term(C)]);
+			case binary_to_term(Data) of
+				0 ->
+					io:format("send chat msg ok...  ~n", []);
+				Chat ->
+					#chat{from = From, c = C} = Chat,
+					io:format("======== chat ======= ~p ::: ~p~n", [binary_to_term(From), binary_to_term(C)])
+			end;
 		_ ->
 			io:format("undefined msg: ~p~n", [Msg])
 	end;
